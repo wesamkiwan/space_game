2d9ec9040f7ffb9b5ecdc7d6c9ca02fca657d280
wesam kiwan <wesamyk84@hotmail.com>
2021-06-11 21:01:40 +0200

    set a ship_speed variable in game settings file

--- ship.py
+++ ship.py
@@ -2,21 +2,26 @@ import pygame
 
 class Ship:
     def __init__(self, my_game):
-        self.screen=my_game.screen 
+        self.screen=my_game.screen
+        self.settings=my_game.settings 
         self.screen_rect=my_game.screen.get_rect()
         self.image=pygame.image.load('images/ship.png')
         self.rect=self.image.get_rect()
         self.rect.midbottom=self.screen_rect.midbottom
+        self.x=float(self.rect.x)
 
         self.moving_right=False
         self.moving_left=False
+
+        
+
 
 
 
     def update(self):
         if self.moving_right:
             self.rect.x+=1
-        elif self.moving_left:
+        if self.moving_left:
             self.rect.x-=1
 
     def draw_ship(self):
