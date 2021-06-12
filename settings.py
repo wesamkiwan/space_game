2b4a160d5784585991e90b3ebbf7f4de10a4ba8f
wesam kiwan <wesamyk84@hotmail.com>
2021-06-12 23:54:26 +0200

    creat bullet.py file and write bullet settings in the settings file

--- settings.py
+++ settings.py
@@ -6,6 +6,12 @@ class Settings:
         #screen settings
         self.screen_width=1200
         self.screen_height=800
-        self.bg_color=(210,210,210)
+        self.bg_color=(0,0,0)
+        #ship settings
         self.ship_speed=1.5
+        #bullet settings
+        self.bullet_color=(255,88,4)
+        self.bullet_height=15
+        self.bullet_width=3
+        self.bullet_speed=1.0
     
