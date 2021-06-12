b52d72cf3b9506b8a76f4766415f60bcc437e2c0
wesam kiwan <wesamyk84@hotmail.com>
2021-06-13 00:08:12 +0200

    writing Bullet class

--- bullet.py
+++ bullet.py
@@ -2,3 +2,15 @@ import pygame
 from pygame.sprite import Sprite
 
 
+class Bullet(Sprite):
+    def __init__(self,SpaceGame):
+        super().__init__()
+        self.screen=SpaceGame.screen
+        self.settings=SpaceGame.settings
+        self.color=self.settings.bullet_color
+
+        self.rect=pygame.Rect(0,0, self.settings.bullet_width, self.settings.bullet.height)
+        self.rect.midtop=SpaceGame.ship.rect.midtop
+
+        self.y=float(self.rect.y)
+        
