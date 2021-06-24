d288f4e53b551f450ce8ffa0aaac9c3d995c88d7
wesam kiwan <wesamyk84@hotmail.com>
2021-06-24 22:15:22 +0200

    issue with the class GameStats is fixed

--- game_stats.py
+++ game_stats.py
@@ -1,7 +1,6 @@
 class GameStats:
-    def __init__(self, my_game):
-        self.setting=my_game.settings
+    def __init__(self,my_game):
+        self.settings=my_game.settings
         self.reset_stats()
-
     def reset_stats(self):
         self.ships_left=self.settings.ship_limit
